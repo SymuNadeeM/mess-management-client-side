7ff6472157bf9e9d02cdeb0793a3055eabb8b428
SymuNadeem <msymunnadeem@gmail.com>
2024-09-04 21:37:16 +0600

    baseurl update

--- src/lib/httpRequest.js
+++ src/lib/httpRequest.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_BASE_URL}`,
+  baseURL: `http://localhost:5000/api`,
   timeout: 50000,
   headers: {
     Accept: 'application/json',
